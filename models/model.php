3247961737e376b9d4512c7d20dfd72c3412b8b5
rezaeimahboobeh <rezaei.mahboobeh@gmail.com>
2018-12-26 11:37:17 +0330

    initialize mvc project

---
+++ models/model.php
@@ -0,0 +1,9 @@
+<?php
+
+//$arrayName = array('book1','book2','book3' );
+
+	$_GET['book']="book";
+
+
+
+?>
---
+++ views/view.php
@@ -0,0 +1,20 @@
+<?php
+echo"<!DOCTYPE html>
+<html>
+<head>
+	<title>libarary</title>
+</head>
+<body>
+<form action='../models/model.php' method='GET'>
+Welcome";  echo $_GET["book"]; echo"<br>
+
+
+</form>
+
+
+
+</body>
+</html>
+";
+?>
+
